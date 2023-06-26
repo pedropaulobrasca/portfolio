1470af6551e24ccc9609a9e541cd3a45e0dd7d8f
Pedro Brasca <pedro.brasca@tinnova.com.br>
2023-06-26 17:38:53 -0300

    feat: added styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import styled from 'styled-components'
+
+const Button = styled.button`
+  color: blue;
+  background-color: #dddddd;
+  border: none;
+  padding: 10px;
+  border-radius: 8px;
+  font-weight: bold;
+  cursor: pointer;
+`
+
 function App() {
+  const handleClickButton = () => {
+    alert(1)
+  }
+
   return (
     <>
       <h1>Teste</h1>
+      <Button onClick={handleClickButton}>Teste</Button>
     </>
   )
 }
