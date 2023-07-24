2dd79da08d8b752e1205d3c059e01360e7e3216a
Pedro Brasca <pedro.brasca@tinnova.com.br>
2023-07-24 20:09:29 -0300

    feat: vercel config

--- src/components/home-page/Header/index.tsx
+++ src/components/home-page/Header/index.tsx
@@ -1,14 +1,16 @@
+import { Link } from "react-router-dom";
 import { ToggleSwitch } from "../ToggleSwitch";
 import { Container } from "./styled";
 
 interface HeaderProps {
   onThemeToggle: () => void;
-  theme: string; // Adicione a propriedade "theme"
+  theme: string;
 }
 
 export const Header = ({ onThemeToggle, theme }: HeaderProps) => {
   return (
     <Container>
+      <Link to="/challenges">.asdasdasd</Link>
       {/* <ToggleSwitch isLanguage /> */}
       <ToggleSwitch
         isTheme
